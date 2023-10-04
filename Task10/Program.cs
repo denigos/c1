399c009771e8c28ed4431f3d223a463e4bf958cb
Denis <denushka.ru@gmail.com>
2023-10-04 20:37:15 +0300

    HomeWork Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿int num = new Random().Next(100, 1000);
+Console.WriteLine($"На вход получаем число => {num}");
+
+
+int DeleteSecondDigit(int number)
+{
+    int firstNum = number / 10 % 10;
+    return firstNum;
+}
+
+Console.WriteLine(DeleteSecondDigit(num));
