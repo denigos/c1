838628034d7046b1509d8a46fb21d1f0c6012931
Denis <denushka.ru@gmail.com>
2023-10-04 20:30:29 +0300

    DeleteSecondDigit

---
+++ Task11/Program.cs
@@ -0,0 +1,12 @@
+﻿int num = new Random().Next(100, 1000);
+Console.WriteLine($"На вход получаем число => {num}");
+
+
+int DeleteSecondDigit(int number)
+{
+    int firstNum = number / 100;
+    int lastNum = number % 10;
+    return firstNum * 10 + lastNum;
+}
+
+Console.WriteLine(DeleteSecondDigit(num));
