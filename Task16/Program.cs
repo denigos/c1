707b44c88140584262ac4c799cad6a8e180188dd
Denis <denushka.ru@gmail.com>
2023-10-05 09:05:15 +0300

    Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Please enter 1 number: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter 2 number: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+bool IsSquareToNumbers(int num1, int num2)
+{
+    return num1 * num1 == num2 || num2 * num2 == num1;
+}
+
+bool result = IsSquareToNumbers(firstNum, secondNum);
+
+Console.WriteLine(result ? $"{firstNum}, {secondNum} => yes" : $"{firstNum}, {secondNum} => no");
