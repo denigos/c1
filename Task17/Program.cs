c0f81a4216d52c0bd5bbc145ae7fa6fa15905049
Denis <denushka.ru@gmail.com>
2023-10-21 12:22:30 +0300

    Task17

---
+++ Task17/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Transactions;
+Console.WriteLine("Введите координаты точек");
+Console.Write("x: ");
+int xCoorinate = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("y: ");
+int yCoorinate = Convert.ToInt32(Console.ReadLine());
+
+int quarter = Quarter(xCoorinate, yCoorinate);
+
+string result = quarter > 0
+? $"Указанные координаты соответствуют четверти {quarter}"
+: "Введены координаты < 0";
+
+Console.WriteLine(result);
+// metod
+int Quarter(int x, int y)
+{
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
+    return 0;
+}
+
+
+
